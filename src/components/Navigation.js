68a7071652061cf2bc4d19c0da8daa38c692206f
samtalebi <samtalebi84@gmail.com>
2020-01-27 10:52:06 +0100

    insert logo image

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import logo from './images/logo.png'
 
 class Navigation extends React.Component{
     render(){
         return(
             
             <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-              
-              <a className="navbar-brand" href="#"><img></img></a>
+              {/* Brand */}
+              <a className="navbar-brand" href="#"><img src={logo} alt="Sams Salat Logo"></img></a>
         
-             
+             {/* collapsing button */}
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span className="navbar-toggler-icon"></span>
               </button>
         
-             
+             {/* Links */}
               <div className="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
                 <ul className="navbar-nav">
                   <li className="nav-item">
