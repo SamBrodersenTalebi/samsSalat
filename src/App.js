2721eaede8be607058bc59b166cfff0abc1451f1
samtalebi <samtalebi84@gmail.com>
2020-01-27 09:51:59 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+//import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*
+BrowserRouter: renamed as Router, gives ability to use routing
+Route: renders componend based on URL
+Switch: as soon as one URL matches it stops searching through the routes
+*/
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          {/* 
+          The exact attribute specifies that the URL
+          needs to be exactly like that for it to be rendered.
+          */}  
+          <Route path="/about" exact component={About}/>
+          <Route path="/contact" exact component={Contact}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
