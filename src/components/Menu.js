f57b409a9638181d55427757ea21797c360b3867
samtalebi <samtalebi84@gmail.com>
2020-03-03 11:02:23 +0100

    responsive menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,6 +16,39 @@ class Menu extends React.Component{
 
         </div>
         <Container id="menu-container">
+        <Row className="align-items-center text-center menu-row">
+          <Col md={6} sm={12} className="menu-image-col">
+            <img className="menu-image" alt="salad image" src={salad}></img>
+          </Col>
+
+          <Col md={6} sm={12}>
+            <h3>Salatbaren</h3>
+            <p className="menu-text">
+              Salatbaren er meget lækker Nunc vitae erat mi. Curabitur luctus, tortor in porttitor semper, lectus neque pulvinar turpis, sit amet aliquet ante felis a mi. Etiam vitae odio eros. 
+              Curabitur rhoncus leo erat, eget mollis leo bibendum eget. Vivamus consectetur ornare convallis. Proin elementum orci vel turpis blandit auctor.
+            </p>
+          </Col>
+
+          <Col sm={12} style={{display:"none"}} className="menu-mobile">
+  
+          </Col>
+        </Row>
+
+        <Row className="align-items-center text-center menu-row">
+
+          <Col md={6} sm={12}>
+            <h3>Om vores sandwich</h3>
+            <p className="menu-text"> 
+              Alle vores sanwich indeholder salat, tomat, agurk, peberfrugt, rucola, krydderurter og rødløg. Det bruges altid friske råvarer osv.
+            </p>
+          </Col>
+
+          <Col md={6} sm={12} className="menu-image-col">
+            <img className="menu-image" alt ="sandwich image" src={sandwich}>
+            </img>
+          </Col>
+        </Row>
+
         <Row>
           <Col md={12}>
             <ol class="rounded-list">
@@ -23,30 +56,7 @@ class Menu extends React.Component{
             </ol>
           </Col>
         </Row>
-        <Row className="align-items-center text-center menu-row">
-          <Col md={6} sm={12}>
-            <img className="menu-image" alt="salad image" src={salad}></img>
-          </Col>
-          <Col md={6} sm={12}>
-            <p className="menu-text">
-              Salatbaren er meget lækker Nunc vitae erat mi. Curabitur luctus, tortor in porttitor semper, lectus neque pulvinar turpis, sit amet aliquet ante felis a mi. Etiam vitae odio eros. 
-              Curabitur rhoncus leo erat, eget mollis leo bibendum eget. Vivamus consectetur ornare convallis. Proin elementum orci vel turpis blandit auctor.
-            </p>
-
-          </Col>
-        </Row>
-
-        <Row className="align-items-center text-center menu-row">
-          <Col md={6} sm={12}>
-            <p className="menu-text"> 
-              Alle vores sanwich indeholder salat, tomat, agurk, peberfrugt, rucola, krydderurter og rødløg. Det bruges altid friske råvarer osv.
-            </p>
-          </Col>
-          <Col md={6} sm={12}>
-            <img className="menu-image" alt ="sandwich image" src={sandwich}>
-            </img>
-          </Col>
-        </Row>
+        
         </Container>
       </div>
       
