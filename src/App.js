46d191498c0136ab97ff4c66360b1b8d301b7218
samtalebi <samtalebi84@gmail.com>
2020-09-29 12:31:24 +0200

    add mundbind warning

--- src/App.js
+++ src/App.js
@@ -6,28 +6,38 @@ BrowserRouter: renamed as Router, gives ability to use routing
 Route: renders componend based on URL
 Switch: as soon as one URL matches it stops searching through the routes
 */
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import AboutUs from './components/AboutUs';
 import Menu from './components/Menu';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import swal from 'sweetalert';
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  componentDidMount() {
+    swal({
+      title: 'Husk Mundbind!',
+      text: 'Påbud fra sundhedsmyndighederne.',
+      icon: 'warning',
+      button: 'OK',
+      dangerMode: true,
+    });
+  }
+  render() {
     return (
       <Router>
-        <div id="App">
+        <div id='App'>
           <Navigation />
           <Switch>
             {/* 
             The exact attribute specifies that the URL
             needs to be exactly like that for it to be rendered.
-            */}  
-            <Route path="/" exact component={Home}/>
-            <Route path="/about" exact component={AboutUs}/>
-            <Route path="/menu" exact component={Menu}/>
+            */}
+            <Route path='/' exact component={Home} />
+            <Route path='/about' exact component={AboutUs} />
+            <Route path='/menu' exact component={Menu} />
           </Switch>
         </div>
         <Footer />
