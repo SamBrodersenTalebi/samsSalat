fe76a90c3bc0a99568068f88a419b9200d25e908
SamBrodersenTalebi <44803892+SamBrodersenTalebi@users.noreply.github.com>
2021-11-14 15:33:10 +0100

    Delete image from carousel

--- src/components/FrontPage/ImageCarousel.js
+++ src/components/FrontPage/ImageCarousel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import forside1 from '../images/sandwichforside.jpg';
-import forside2 from '../images/TaSelv.jpg';
 import forside3 from '../images/salatbar1.jpg';
 
 class ImageCarousel extends React.Component {
@@ -10,10 +9,6 @@ class ImageCarousel extends React.Component {
       <Carousel>
         <Carousel.Item>
           <img className='carousel' src={forside1} alt='First slide' />
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img className='carousel' src={forside2} alt='Second slide' />
         </Carousel.Item>
         <Carousel.Item>
           <img className='carousel' src={forside3} alt='Third slide' />
