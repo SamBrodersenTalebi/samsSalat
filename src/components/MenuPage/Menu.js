b388e39a620c4870347603d7261aa0557da7c1b5
SamBrodersenTalebi <44803892+SamBrodersenTalebi@users.noreply.github.com>
2022-08-02 15:02:56 +0200

    Update prices

--- src/components/MenuPage/Menu.js
+++ src/components/MenuPage/Menu.js
@@ -119,9 +119,9 @@ class Menu extends React.Component {
             <Col md={6} sm={12}>
               <h3>Salat og Sandwich</h3>
               <p className='menu-text'>
-                Prisen for en salat er <strong>12.95 kr per 100 gram </strong>{' '}
+                Prisen for en salat er <strong>13.95 kr per 100 gram </strong>{' '}
                 <br />
-                Prisen for en sandwich er fra <strong>58 kr</strong> <br />
+                Prisen for en sandwich er fra <strong>62 kr</strong> <br />
                 <br />
                 Det er nemt, sundt og hurtigt at blande sin egen salat. Det er
                 op til dig selv at sammensætte din helt egen lækre salat. <br />{' '}
